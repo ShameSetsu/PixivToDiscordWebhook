2157bfc23a8e22e884cf5994424370c8cac1ac3d
ShameiSetsu <shameimaru.setsuri@gmail.com>
2018-03-15 19:58:03 +0100

    Get the full size picture instead of the 240*240 public format

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 +# PixivToDiscordWebhook
 +Simple nodejs script to search latest pictures from Pixiv (given a tag) and push them to a discord webhook
 
-include a login.json file at the root with this format:
+WARNING: Pixiv contains NSFW content. Maybe using an account configured to hide that king of content would fix that or optionnal headers can be included in the search request. You can also filter the result to exclude pictures containing the tag 'R-18'.
 
-{
-    "pixiv_email": "email@gmail.com",
-    "pixiv_password": "myPassword",
-    "discord_webhook": "https://discordapp.com/api/webhooks/S0M3R4ND0M/STR1NG"
+include a login.json file at the root with this format:
+  
+{  
+    "pixiv_email": "email@gmail.com",  
+    "pixiv_password": "myPassword",  
+    "discord_webhook": "https://discordapp.com/api/webhooks/S0M3R4ND0M/STR1NG"  
 }
--- webhook.js
+++ webhook.js
@@ -1,7 +1,6 @@
 const request = require('request');
 const PixivApi = require('pixiv-api-client');
 const pixiv = new PixivApi();
-const fs = require('fs');
 
 // CONFIG
 const 
@@ -10,32 +9,52 @@ pixiv_password = require('./login.json').pixiv_password,                        
 discord_webhook = require('./login.json').discord_webhook,                                      // discord webhook url
 search = '射命丸文',                                                                            // search request
 avatar_url = 'https://pbs.twimg.com/profile_images/846488696958451712/MJjGGlwD_400x400.jpg',    // bot avatar url
-bot_username = 'Pixiv Aya';                                                                     // bot username
+bot_username = 'Ayaya';                                                                         // bot username
 
-function postToDiscord(image_url){
+function postToDiscord(picture, filename, username){ // Post the picture to discord
     
-    request.post({
-        url: discord_webhook,
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: `{
-            "username": "` + bot_username +`",
-            "avatar_url": "` + avatar_url + `",
-            "content": "` + image_url + `"
-        }`
-    }, (err, res, body)=>{
-        console.log('body', body);
+    var formData = {
+        content: "New picture from " + username + " !",
+        username: bot_username,
+        avatar_url: avatar_url,
+        embeds: [{
+            value: picture,
+            options: {
+                filename:  filename,
+                contentType: 'image/jpeg'
+            }
+        }]
+    };
+    request.post({url: discord_webhook, formData: formData}, (err, res, body)=>{
+        console.log('DONE');
     });
 }
 
-pixiv.login(pixiv_email, pixiv_password).then(()=>{
-    return pixiv.searchIllust(search).then(json=>{
-
-        let image_uri = json.illusts[0].image_urls.medium.replace('540x540_70', '240x240');
-
-        postToDiscord(image_uri);
+function downloadPicture(uri, filename, username, callback){ // Download the picture from pixiv
+    request.head(uri, (err, res, body)=>{
+        options = {
+            url: uri,
+            headers: {
+                "Referer": "http://www.pixiv.net/"
+            },
+            encoding: null // important: prevent from returning 'utf-8' encoded string
+        };
         
-        return;
+        request.get(options, (res, err, picture)=>{
+            postToDiscord(picture, filename, username);
+        });
+    });
+}
+
+pixiv.login(pixiv_email, pixiv_password).then(()=>{ // Get the latest picture with a given tag
+    return pixiv.searchIllust(search).then(json=>{
+        
+        let image_name = json.illusts[0].title + '.jpg';
+        let image_uri = json.illusts[0].image_urls.medium.replace('c/540x540_70/', ''); // Format the url to access the full size picture
+        let username = json.illusts[0].user.name;
+
+        downloadPicture(image_uri, image_name, username, ()=>{
+            console.log(done);
+        });
     });
 });
